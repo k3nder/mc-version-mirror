2e789793237a6192a31e26d1d66c53428614cb34
k3nder <k3nder@outlook.es>
2024-10-10 20:08:35 +0200

    update system

---
+++ upload.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+archivo="index"
+
+if [[ -f $archivo ]]; then
+    # Leer el archivo línea por línea
+    while IFS= read -r linea; do
+        echo "Index version: $linea"
+
+	./mclr $linea
+	
+	gh release upload versions $linea.tar.gz
+
+	rm "$linea.tar.gz"
+
+    done < "$archivo"
+else
+    echo "El archivo no existe."
+fi
+
+rm -rf versions
+rm -rf java
